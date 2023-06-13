39bb617219972a82aa3f45dfa833262a55ce2a41
Abdul Aziz <doelziez212@gmail.com>
2023-06-13 22:22:02 +0700

    #1 frontend & backend

---
+++ backend/helpers/jwt.js
@@ -0,0 +1,36 @@
+const { expressjwt : jwt } = require('express-jwt');
+
+function authJwt() {
+    const secret = process.env.secret;
+    const api = process.env.API_URL;
+
+    return jwt({
+        secret,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
+    }).unless({
+        path: [
+            {url: /\/api\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`,
+        ]
+    })
+}
+// async function isRevoked(req, payload, done) {
+//     if(!payload.isAdmin) {
+//         done(null, true)
+//     }
+//     done();
+// }
+async function isRevoked(req, payload) {
+    // console.log(payload);
+    if (payload.isAdmin == false) {
+      console.log('Not Admin');
+      return true;
+    }
+    console.log('Admin');
+    return false;
+}
+
+module.exports = authJwt;
